07fd115fde9c1e370a2444350b4663afb6b64f4d
Bootcamp-tk <wboard656@gmail.com>
2025-10-29 09:20:38 +0530

    Add files via upload

---
+++ JavaScript/Arrays.js
@@ -0,0 +1,20 @@
+//Syntax
+// let arrayName = [value1,  value2,  value3, value4, ...]
+
+// let frutit1 = "mango"
+// let frutit2 = "strawb"
+// let frutit3 = "orange"
+
+let fruits = ["Mango", "Strawberry", "Orange", "Apple"]
+// console.log(fruits[2])
+// console.log(fruits[3])
+// // console.log(fruits.length)
+// fruits[1] = "Grapes"
+// console.log(fruits)
+
+// for(let i=0; i<fruits.length; i++){
+//     console.log(`The fruit at index ${i} is ${fruits[i]}`)
+// }
+// for(let fruit of fruits){
+//     console.log(fruit.toUpperCase())
+// }
